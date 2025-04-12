60b211c0131532893e375fa4b81bbc416ad452a8
s3dallaz <abdelrahman.pro9@gmail.com>
2025-04-12 14:46:25 +0200

    Python Tic-Tac-Toe.py

--- Python Tic-Tac-Toe.py
+++ Python Tic-Tac-Toe.py
@@ -1,11 +1,11 @@
 def display_board(board):
-    print("---------")
+    print("--------------")
     print(f"| {board[0]} | {board[1]} | {board[2]} |")
-    print("---------")
+    print("--------------")
     print(f"| {board[3]} | {board[4]} | {board[5]} |")
-    print("---------")
+    print("--------------")
     print(f"| {board[6]} | {board[7]} | {board[8]} |")
-    print("---------")
+    print("--------------")
 
 def get_player_move(board, player):
     while True:
